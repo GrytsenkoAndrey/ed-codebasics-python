d06cc3e946cfcd8626fda6d33b69f78c628dce48
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-21 13:33:16 +0300

    D1, input & output string 2024-07-21

--- Specialist/01-base-level/demo.py
+++ Specialist/01-base-level/demo.py
@@ -7,8 +7,21 @@ world!'''
 # но этот способ не используется, а используется вот такой способ
 n = 'Hello, \
 world!'
-print(n)
+#print(n)
 
 #####
-number = input('Press your number:')
-print('Your number:', number)
+#number = input('Press your number:')
+#print('Your number:', number)
+
+# use placeholder
+name = input('Enter your name:')
+age = input('Enter your age:')
+print('Your name is %s, your age is %s' % (name, age))
+# second variant
+result = 'Your name is {}, your age is {}'.format(name, age)
+print(result)
+# в третьем варианте можно указать порядок
+res = 'Your name is {1}, your age is {0}'.format(age, name)
+print(res)
+formstring = f'Your name is {name}, your age is {age}'
+print(formstring)

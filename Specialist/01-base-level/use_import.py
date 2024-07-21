9e590ddfa8ac8d071d50cbdb732dbfe7c4fc792f
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-21 14:06:27 +0300

    D1, use import, check input data 2024-07-21

--- Specialist/01-base-level/use_import.py
+++ Specialist/01-base-level/use_import.py
@@ -3,7 +3,11 @@ import random
 n1 = random.randint(1, 100)
 n2 = random.randint(1, 100)
 result = input(f'Сколько будет {n1} + {n2}? ')
-if int(result) == n1 + n2:
-    print('Верно!')
+
+if result.isdigit():
+    if int(result) == n1 + n2:
+        print('Верно!')
+    else:
+        print('Неверно!')
 else:
-    print('Неверно!')
+    print('Введите число!')

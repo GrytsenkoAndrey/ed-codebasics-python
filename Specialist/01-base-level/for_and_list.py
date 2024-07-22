cebb520d9be185b263a1d1179287fa0ed3953d29
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-22 18:46:19 +0300

    D1, for, range and list 2024-07-22

---
+++ Specialist/01-base-level/for_and_list.py
@@ -0,0 +1,17 @@
+# for i in range(1, 11):
+#     print(i)
+
+# l = [1,2,3,4,5]
+#
+# for i in l:
+#     print(i)
+#
+# l.append(7)
+# print(l[3])
+#
+# l[4] = 6
+
+r = range(6)
+print(r[2])
+p = list(r)
+print(p)

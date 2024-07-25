d4eaf8fa51aadbf8e03259173ffdcfd1206e9952
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-25 19:38:07 +0300

    D1, import modules 2024-07-25

---
+++ Specialist/01-base-level/use_imports.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.sqrt(16))  # 4.0
+print(math.cos(1.0))  # 0.5403023058681398
+print(math.pi)  # 3.141592653589793
+print(math.e)  # 2.718281828459045
+print(math.sin(1.0))  # 0.8414709848078965
+print(math.tan(1.0))  # 1.5574077246549023
+
+## how to know that what are the functions available in module
+# dir(math)
+# функции с __ в начале и конце - это внутренние функции, их не стоит использовать
+
+print(math.gcd(12, 15))  # 3

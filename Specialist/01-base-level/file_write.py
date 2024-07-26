b778bd3126a55122d5e764d446e4e2fc7bcf2231
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-26 19:37:03 +0300

    D1, file - write 2024-07-26

---
+++ Specialist/01-base-level/file_write.py
@@ -0,0 +1,8 @@
+# with open('test.txt', 'a', encoding='utf-8') as f:
+#     f.write('\nHello, World!\n')
+
+# при r+ можно сказать, что включен режим Insert
+
+# f.closed - проверка закрыт ли файл
+# f.mode - режим работы с файлом
+# f.name - имя файла

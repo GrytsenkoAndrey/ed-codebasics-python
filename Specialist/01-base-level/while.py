ba385aadd53441398787e14a4b8933693858b9b9
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-26 19:00:34 +0300

    D1, while 2024-07-26

---
+++ Specialist/01-base-level/while.py
@@ -0,0 +1,23 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+print('---------')
+
+j = 1
+while j < 6:
+    print(j)
+    if j == 3:
+        break
+    j += 1
+print('---------')
+
+
+k = 1
+while k < 6:
+    k += 1
+    if k == 3:
+        continue
+    print(k)
+print('---------')
+

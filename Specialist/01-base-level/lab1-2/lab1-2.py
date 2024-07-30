3ee1df7866678a50b071accbb68dcf4d74c23b2f
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-30 09:12:24 +0300

    D1, lab1-2 update 2024-07-30

--- Specialist/01-base-level/lab1-2/lab1-2.py
+++ Specialist/01-base-level/lab1-2/lab1-2.py
@@ -2,9 +2,9 @@ import read_file
 import save_file
 
 l = read_file.read_file()
-count = len(l)
 save_file.save_file('result.txt', l)
-print('# Всего уникальных слов: ', count)
+
+print('# Всего уникальных слов: ', len(l))
 print('===============================')
 print('# Cписок уникальных слов:')
 for i in l:

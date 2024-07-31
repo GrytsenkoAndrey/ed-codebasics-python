31596c3fcf126916581d9ff9216bd770c4beded9
Andrey Grytsenko <andreyapg@gmail.com>
2024-07-31 11:23:44 +0300

    D1, exceptions p2 2024-07-31

--- Specialist/01-base-level/exceptions.py
+++ Specialist/01-base-level/exceptions.py
@@ -1,4 +1,9 @@
-print(1)
-input('...'
+# print(1)
+# input('...'
 
 # SyntaxError: unexpected EOF while parsing
+
+#==================================================================================================
+
+x = 'a' * 'b'
+# TypeError: can't multiply sequence by non-int of type 'str'
